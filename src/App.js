da00d3546e3afe0746fe713b5cb640d4b452aefc
RohitoOo <bhambhani.ba@gmail.com>
2018-05-20 13:54:10 +0530

    this.States

--- src/App.js
+++ src/App.js
@@ -2,51 +2,66 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Me from './Me';
+import Counter from './Counter';
 
-function App({data}){
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={data.image} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to Reactions Mr. {data.firstName}</h1>
-      </header>
-      <p className="App-intro">
-        Journey of React begins....
-      </p>
-      <div>
 
-          <Me data={data} />
+// function App({data}){
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={data.image} className="App-logo" alt="logo" />
+//         <h1 className="App-title">Welcome to Reactions Mr. {data.firstName}</h1>
+//       </header>
+//       <p className="App-intro">
+//         Journey of React begins....
+//       </p>
+//       <div>
+//
+//           <Me data={data} />
+//
+//       </div>
+//
+//     </div>
+//
+//   )
+// }
+
+class App extends Component {
+
+
+      constructor(props) {
+          super(props);
+          this.state = {
+            showCounter : true
+          }};
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={this.props.data.image} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Reactions Mr. {this.props.data.firstName}</h1>
+        </header>
+        <p className="App-intro">
+          Journey of React begins....
+        </p>
+        <div>
+        <Counter />
+
+
+        </div>
+        <div>
+
+            <Me data={this.props.data} />
+
+        </div>
 
       </div>
 
-    </div>
-
-  )
+    )
+  }
 }
 
 
-
-
 export default App;
-
-
-
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           Journey of React begins....
-//         </p>
-//       </div>
-//
-//
-//
-//     );
-//   }
-// }
---
+++ src/Counter.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react' ;
+
+class Counter extends Component {
+
+  constructor (props) {
+    super(props);
+
+      this.state = {
+        autoClickedIncrement : false,
+        count: 0,
+        autoClickedDecrement : false
+      }
+
+
+  }
+
+
+ incrementFunction = () => {
+
+
+
+  this.setState(function (currentState){
+    return {count : currentState.count + 1  }
+  })
+}
+
+decrementFunction =  () => {
+
+  this.setState(function (currentState) {
+    return { count : currentState.count - 1}
+  })
+}
+
+autoIncrement = () => {
+
+if(!this.state.autoClickedIncrement) {
+
+
+setInterval(   () => {
+
+ this.setState(function (currentState){
+   return {count : currentState.count + 1  , autoClickedIncrement: true }
+ })
+}     , 1000 ) }
+
+}
+
+autoDecrement = () => {
+
+if(!this.state.autoClickedDecrement) {
+
+  setInterval(  () => {
+    this.setState((currentState) => {
+  return   {count : currentState.count - 1 , autoClickedDecrement : true}
+  })}    , 1000)
+}
+}
+
+
+
+
+render(){
+  return (
+    <div>
+      {this.state.count}
+      <button onClick={this.incrementFunction} >Increment</button>
+      <button onClick={this.decrementFunction} >Decrement</button>
+      <button onClick={this.autoIncrement}> Auto Increment </button>
+      <button onClick={this.autoDecrement}> Auto Decrement </button>
+
+
+    </div>
+  )
+}
+
+
+
+
+}
+
+export default Counter;
